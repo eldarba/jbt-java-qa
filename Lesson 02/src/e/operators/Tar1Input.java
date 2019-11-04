92541fe4809880f4a9785434cc1124dad3703397
jbt <jbt@192.168.0.132>
2019-11-04 21:05:42 +0100

    aaa

--- Lesson 02/src/e/operators/Tar1Input.java
+++ Lesson 02/src/e/operators/Tar1Input.java
@@ -7,6 +7,18 @@ public class Tar1Input {
 	public static void main(String[] args) {
 		// create an object to get input from keyboard
 		Scanner sc = new Scanner(System.in);
+		
+		// prompt user for input
+		System.out.print("enter a number: ");
+		
+		// get the input from user and put in variable x
+		int x = sc.nextInt();
+		
+		// print the user inputs
+		System.out.println(x);
+		
+		// close the scanner
+		sc.close();
 	}
 
 }
